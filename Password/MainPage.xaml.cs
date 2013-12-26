084aa7a1df3a2e3b54a4852c879fc67017237538
kupa25 <kupa25@live.com>
2013-12-26 15:59:16 -0500

    Initial commit
    Blank App, with Bottom Bar and the idea of adding a password dialog box

---
+++ Password/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Password
+{
+    using Windows.UI.Popups;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.AddPassword.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddPasswordBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.AddPassword.Visibility = this.AddPassword.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.AddPassword.Visibility = Visibility.Collapsed;
+        }
+    }
+}
