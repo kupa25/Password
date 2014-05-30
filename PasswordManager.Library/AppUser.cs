74663970b3bf13aea1b4b7619bde29b04722de5a
kupa25@gmail.com <haran802>
2014-05-29 21:18:58 -0400

    Revert "Converting the project to Universal app project"
    This reverts commit dd2b3f26fea260f9283f91d3df20891091454352.

---
+++ PasswordManager.Library/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace PasswordManager.Library
+{
+    public class AppUser
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "DeviceId")]
+        public string DeviceId { get; set; }
+
+        [JsonProperty(PropertyName = "SerializedPassword")]
+        public string SerializedPassword { get; set; }
+    }
+}
---
+++ PasswordManager.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Phone.Controls;
+using Microsoft.WindowsAzure.MobileServices;
+using PasswordManager.Library;
+
+
+namespace PasswordManager.WindowsPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private IMobileServiceTable<AppUser> AppUserTable = App.MobileService.GetTable<AppUser>(); 
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //IList<Password> tempList = new List<Password>();
+
+            //tempList.Add(new Password{ Title = "Kshitij", UserName = "Kupa", PasswordText = "test"});
+
+            LoadFromMobileService();
+
+            //ListSelector.ItemsSource = (IList) tempList;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private async Task LoadFromMobileService()
+        {
+            var phoneUser = new AppUser
+            {
+                DeviceId = "test2",
+                Id = "test2",
+                SerializedPassword = "test2"
+            };
+
+            await AppUserTable.InsertAsync(phoneUser);
+
+            return;
+        }
+    }
+}
